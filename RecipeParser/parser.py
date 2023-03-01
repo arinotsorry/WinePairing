9bb4da0cc637e652c3c91802cf8f0b4718f2fe46
Ari Wisenburn <>
2023-03-01 13:12:38 -0500

    parser returns 'lemmatized' tokens with pos tags

--- RecipeParser/parser.py
+++ RecipeParser/parser.py
@@ -3,16 +3,28 @@ Lazy parser for recipes.
 Uses Natural Language Tool Kit
 Ideally want to use BERT and some language things one day, but not right now
 """
-
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 import json
 import os
 from os import listdir
 
-nltk.download( 'stopwords' )
-
+WORDNET_TAGS = {  # 'n, s, a, r, v'
+    "NN" : 'n',
+    "NNS": 'n',
+    "VBZ": 'v',
+    "VBP": 'v',
+    "VBN": 'v',
+    "VBG": 'v',
+    "VBD": 'v',
+    "VB" : 'v',
+    "JJ" : 'a',
+    "JJR": 'a',
+    "JJS": 'a',
+    "RB" : 'r',
+}
 POS = {
     "NNP" : 'proper noun, singular (sarah)',
     "TO"  : 'infinite marker (to)',
@@ -62,16 +74,29 @@ def main():
         content = file.read()
         
         # get tokens
-        tokens = nltk.word_tokenize( content )
+        tokens = nltk.word_tokenize( content.lower() )
         
         # remove stopwords from token set
         tokens = [ token for token in tokens if token not in set( stopwords.words( 'english' ) ) ]
         
+        # lemmaatization - it sort of works
+        lemmatizer = WordNetLemmatizer()
+        
         tagged = nltk.pos_tag( tokens )
         print( tagged )
         
-        # count and sort the words
+        lemmatized_tokens = [ ]
+        
+        # lemmatize all the words - this is more effective with the POS info along with the word
         for pair in tagged:
+            if pair[ 1 ] in WORDNET_TAGS:
+                lemmatized_tokens.append( lemmatizer.lemmatize( pair[ 0 ], WORDNET_TAGS[ pair[ 1 ] ] ) )
+        
+        # tag the lemmatized words
+        lemmatized_tagged = nltk.pos_tag( lemmatized_tokens )
+        
+        # count and sort the lemmatized/tagged tokens
+        for pair in lemmatized_tagged:
             if pair[ 1 ] in words:
                 if pair[ 0 ] in words[ pair[ 1 ] ]:
                     words[ pair[ 1 ] ][ pair[ 0 ] ] = words[ pair[ 1 ] ][ pair[ 0 ] ] + 1
@@ -79,10 +104,10 @@ def main():
                     words[ pair[ 1 ] ][ pair[ 0 ] ] = 1
             else:
                 words[ pair[ 1 ] ] = { pair[ 0 ]: 1 }
-    
-    file2 = open( './pos.json', 'w' )
-    words_json = json.dumps( words, indent = 2 )
-    file2.write( words_json )
+        
+        file2 = open( './pos.json', 'w' )
+        words_json = json.dumps( words, indent = 2 )
+        file2.write( words_json )
 
 
 main()
---
+++ RecipeParser/process.md
@@ -0,0 +1,8 @@
+How the parser works:
+
+- paste in recipes (copy-pasted from allrecipes.com), I have them all in another directory
+- convert to lowercase and tokenize
+- remove stopwords
+- lemmatize tokens
+- do POS tagging
+- 
